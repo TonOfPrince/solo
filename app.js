8cd2d3a4102055dc8b617b9fe78aa45e0e4f1071
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2014-12-01 21:10:41 -0800

    removePose works

--- app.js
+++ app.js
@@ -127,102 +127,130 @@ angular.module('yogauto', [])
 	    type: ['ARM BALANCES'],
 	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/eightangle.jpg'
 	  },
-	  // {
-	  //   english: "Extended Hand-To-Big-Toe Pose",
-	  //   sanskrit: "Utthita Hasta Padangustasana",
-	  //   type: ['BALANCING', 'HIP OPENERS', 'STANDING POSES']
-	  // },
-	  // {
-	  //   english: "Extended Puppy Pose",
-	  //   sanskrit: "Uttana Shishosana",
-	  //   type: ['FORWARD BENDS']
-	  // },
-	  // {
-	  //   english: "Extended Side Angle Pose",
-	  //   sanskrit: "Utthita Parsvakonasana",
-	  //   type: ['STANDING POSES', 'YOGA FOR STRENGTH']
-	  // },
-	  // {
-	  //   english: "Extended Triangle Pose",
-	  //   sanskrit: "Utthita Trikonasana",
-	  //   type: ['STANDING POSES', 'YOGA FOR STRENGTH']
-	  // },
-	  // {
-	  //   english: "Feathered Peacock Pose",
-	  //   sanskrit: "Pincha Mayurasana",
-	  //   type: ['INVERSIONS', 'YOGA FOR STRENGTH']
-	  // },
-	  // {
-	  //   english: "Fire Log Pose",
-	  //   sanskrit: "Agnistambhasana",
-	  //   type: ['HIP OPENERS', 'SEATED']
-	  // },
-	  // {
-	  //   english: "Firefly Pose",
-	  //   sanskrit: "Tittibhasana",
-	  //   type: ['ARM BALANCES']
-	  // },
-	  // {
-	  //   english: "Fish Pose",
-	  //   sanskrit: "Matsyasana",
-	  //   type: ['BACKBENDS', 'CHEST OPENERS']
-	  // },
-	  // {
-	  //   english: "Four-Limbed Staff Pose",
-	  //   sanskrit: "Chaturanga Dandasana",
-	  //   type: ['ARM BALANCES', 'CORE YOGA', 'YOGA FOR STRENGTH']
-	  // },
-	  // {
-	  //   english: "Garland Pose",
-	  //   sanskrit: "Malasana",
-	  //   type: ['STANDING POSES']
-	  // },
-	  // {
-	  //   english: "Gate Pose",
-	  //   sanskrit: "Parighasana",
-	  //   type: ['STANDING POSES']
-	  // },
-	  // {
-	  //   english: "Half Frog Pose",
-	  //   sanskrit: "Ardha Bhekasana",
-	  //   type: ['BACKBENDS', 'CHEST OPENERS']
-	  // },
-	  // {
-	  //   english: "Half Lord of the Fishes Pose",
-	  //   sanskrit: "Ardha Matsyendrasana",
-	  //   type: ['HIP OPENERS', 'SEATED', 'TWISTS']
-	  // },
-	  // {
-	  //   english: "Half Moon Pose",
-	  //   sanskrit: "Ardha Chandrasana",
-	  //   type: ['BALANCING', 'STANDING POSES']
-	  // },
-	  // {
-	  //   english: "Handstand",
-	  //   sanskrit: "Adho Mukha Vrksasana",
-	  //   type: ['BALANCING', 'INVERSIONS', 'YOGA FOR STRENGTH']
-	  // },
-	  // {
-	  //   english: "Happy Baby Pose",
-	  //   sanskrit: "Ananda Balasana",
-	  //   type: ['CORE YOGA']
-	  // },
-	  // {
-	  //   english: "Head-to-Knee Forward Bend",
-	  //   sanskrit: "Janu Sirsasana",
-	  //   type: ['FORWARD BENDS', 'SEATED']
-	  // },
-	  // {
-	  //   english: "Hero Pose",
-	  //   sanskrit: "Virasana",
-	  //   type: ['SEATED']
-	  // }
+	  {
+	    english: "Extended Hand-To-Big-Toe Pose",
+	    sanskrit: "Utthita Hasta Padangustasana",
+	    type: ['BALANCING', 'HIP OPENERS', 'STANDING POSES'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/yogapedia_270_08_450x450.jpg'
+	  },
+	  {
+	    english: "Extended Puppy Pose",
+	    sanskrit: "Uttana Shishosana",
+	    type: ['FORWARD BENDS'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/266_feat_tri03_4501.jpg'
+	  },
+	  {
+	    english: "Extended Side Angle Pose",
+	    sanskrit: "Utthita Parsvakonasana",
+	    type: ['STANDING POSES', 'YOGA FOR STRENGTH'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/extendedsideanglepose.jpg'
+	  },
+	  {
+	    english: "Extended Triangle Pose",
+	    sanskrit: "Utthita Trikonasana",
+	    type: ['STANDING POSES', 'YOGA FOR STRENGTH'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/266_hp_side16_4501.jpg'
+	  },
+	  {
+	    english: "Feathered Peacock Pose",
+	    sanskrit: "Pincha Mayurasana",
+	    type: ['INVERSIONS', 'YOGA FOR STRENGTH'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/featheredpeacock2.jpg'
+	  },
+	  {
+	    english: "Fire Log Pose",
+	    sanskrit: "Agnistambhasana",
+	    type: ['HIP OPENERS', 'SEATED'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/firelogpose.jpg'
+	  },
+	  {
+	    english: "Firefly Pose",
+	    sanskrit: "Tittibhasana",
+	    type: ['ARM BALANCES'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/kathrynbudigfireflypose.jpg'
+	  },
+	  {
+	    english: "Fish Pose",
+	    sanskrit: "Matsyasana",
+	    type: ['BACKBENDS', 'CHEST OPENERS'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/fishpose.jpg'
+	  },
+	  {
+	    english: "Four-Limbed Staff Pose",
+	    sanskrit: "Chaturanga Dandasana",
+	    type: ['ARM BALANCES', 'CORE YOGA', 'YOGA FOR STRENGTH'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/chaturangafourlimbedstaff.jpg'
+	  },
+	  {
+	    english: "Garland Pose",
+	    sanskrit: "Malasana",
+	    type: ['STANDING POSES'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/252_hp_move_10a_450.jpg'
+	  },
+	  {
+	    english: "Gate Pose",
+	    sanskrit: "Parighasana",
+	    type: ['STANDING POSES'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/266_hp_side15_4501.jpg'
+	  },
+	  {
+	    english: "Half Frog Pose",
+	    sanskrit: "Ardha Bhekasana",
+	    type: ['BACKBENDS', 'CHEST OPENERS'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/2767-1011.jpg'
+	  },
+	  {
+	    english: "Half Lord of the Fishes Pose",
+	    sanskrit: "Ardha Matsyendrasana",
+	    type: ['HIP OPENERS', 'SEATED', 'TWISTS'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/lordofthefisheshalf.jpg'
+	  },
+	  {
+	    english: "Half Moon Pose",
+	    sanskrit: "Ardha Chandrasana",
+	    type: ['BALANCING', 'STANDING POSES'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/halfmoonpose.jpg'
+	  },
+	  {
+	    english: "Handstand",
+	    sanskrit: "Adho Mukha Vrksasana",
+	    type: ['BALANCING', 'INVERSIONS', 'YOGA FOR STRENGTH'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/HandstandPose.jpg'
+	  },
+	  {
+	    english: "Happy Baby Pose",
+	    sanskrit: "Ananda Balasana",
+	    type: ['CORE YOGA'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/249_hp_groove_16_450.jpg'
+	  },
+	  {
+	    english: "Head-to-Knee Forward Bend",
+	    sanskrit: "Janu Sirsasana",
+	    type: ['FORWARD BENDS', 'SEATED'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/251_hp_calm_10_450.jpg'
+	  },
+	  {
+	    english: "Hero Pose",
+	    sanskrit: "Virasana",
+	    type: ['SEATED'],
+	    picture: 'http://d3v7xustcq7358.cloudfront.net/wp-content/uploads/heropose.jpg'
+	  }
 	];
 })
 .controller('poseCtrl', function($scope, Counter) {
+	$scope.practicing = true;
+	$scope.paused = true;
+	$scope.nextable = false;
+	$scope.started = false;
+	$scope.listed = false;
 	$scope.poses = Counter.poses;
 	$scope.randomPosesList = [];
 	$scope.randomPoses = function(number) {
+		$scope.practicing = false;
+		$scope.listed = true;
+		$scope.started = false;
+		$scope.paused = true;
+		$scope.nextable = false;
 		if ($scope.transition) {
 			clearInterval($scope.transition);
 			$scope.current = null
@@ -243,17 +271,29 @@ angular.module('yogauto', [])
 		}
 	};
 	$scope.practicePoses = function() {
+		$scope.practicing = true;
+		$scope.paused = false;
+		$scope.nextable = true;
+		$scope.started = true;
 		$scope.current = $scope.randomPosesList[0];
 		$scope.transition = setInterval(function() {
 			$scope.randomPosesList.shift(); 
 			$scope.current = $scope.randomPosesList[0];
-			$scope.$apply();
 			if ($scope.randomPosesList.length === 0) {
+				$scope.practicing = true;
+				$scope.paused = true;
+				$scope.nextable = false;
+				$scope.started = false;
+				$scope.listed = false;
 				clearInterval($scope.transition);
 			}
+			$scope.$apply();
 		}, 2000);
 	};
 	$scope.pausePoses = function() {
+		$scope.practicing = false;
+		$scope.paused = true;
+		$scope.nextable = false;
 		clearInterval($scope.transition);
 		$scope.transition = null;
 	}
@@ -261,6 +301,9 @@ angular.module('yogauto', [])
 		clearInterval($scope.transition);
 		$scope.randomPosesList.shift(); 
 		$scope.practicePoses();
+	}
+	$scope.removePose = function(index) {
+		$scope.randomPosesList.splice(index, 1);
 	}
 });	
 	
